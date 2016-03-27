fa661db495a86d80025b3ec91b19cda714b9ab8e
Chris Howe-Jones <chris.howejones@gmail.com>
2016-03-27 11:47:25 +0100

    Fixed url in README

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject blogcljsfigwheel "0.1.0-SNAPSHOT"
   :description "Example build using cljs and figwheel"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/chrishowejones/blogcljsfigwheel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
